3c8c8309900d5be223089b2a1d1ead49f12c2323
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-06 18:00:27 +0530

    Added code for rotating matrix to 90 Degrees

---
+++ 01 - Array/27 - Rotate Matrix 90 Degrees.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; ++i){
+            for(int j = 0; j<matrix[0].length; ++j){
+                if(i < j){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<matrix.length; ++i){
+            int start = 0;
+            int end = matrix[0].length-1;
+            while(start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                ++start;
+                --end;
+            }
+        }
+    }
+}
+
+// TC - O(N^2) SC - O(N)
