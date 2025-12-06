3476bce723ec508a61e1b6e48754f8d9f2d441ad
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-06 11:54:08 +0530

    Added code for longest subArray sum for positive, negative numbers

---
+++ 01 - Array/14 - Longest SubArray With Sum K(Positive+Negative).java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+        int max = 0;
+        for(int i = 0; i<arr.length; ++i){
+            prefixSum += arr[i];
+            if (prefixSum == k) {
+                max = Math.max(max, i + 1);
+            }
+            if(map.containsKey(prefixSum - k)){
+                int length = i - map.get(prefixSum - k);
+                max = Math.max(max, length);
+            }
+            if (!map.containsKey(prefixSum)) {
+                map.put(prefixSum, i);
+            }
+        }
+        return max;
+    }
+}
