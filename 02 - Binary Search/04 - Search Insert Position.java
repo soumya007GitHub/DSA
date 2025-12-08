1891a0d606808592f9a229559e7b7f934b6da03a
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-08 19:06:21 +0530

    Added code for search insert position lc 35

---
+++ 02 - Binary Search/04 - Search Insert Position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+      int start = 0;
+      int end = nums.length-1;
+      int ans = nums.length;
+      while(start <= end){
+        int mid = (start+end)/2;
+        if(nums[mid] >= target){
+            ans = mid;
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+      }
+      return ans;
+    }
+}
+
+// Time Complexity - O(logN)
+// Space Complexity - O(1)
