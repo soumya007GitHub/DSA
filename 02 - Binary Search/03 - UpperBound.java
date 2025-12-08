f61a5eb3de1819400892d3a8070ca65a37ce5c6f
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-08 15:21:41 +0530

    Added code for upper bound

---
+++ 02 - Binary Search/03 - UpperBound.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int upperBound(int[] nums, int x) {
+        int low = 0;
+        int high = nums.length-1;
+        int ans = nums.length;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] > x){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
+
+
+// Time Complexity - O(LOG N) Space Complexity - O(1)
