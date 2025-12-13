fd8cf19e1ea652bd693ef0d5b62f1748d2c34699
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-13 20:35:01 +0530

    finding sqrt of a number using binary search

---
+++ 02 - Binary Search/14 - SQRT of number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int floorSqrt(int n) {
+      int low = 1;
+      int high = n;
+      int ans = 1;
+      while(low <= high){
+        int mid = (low+high)/2;
+        if(mid*mid <= n){
+            ans = mid;
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+      }
+      return ans;
+    }
+}
+
+// TC - O(LogN)
+// SC - O(1)
