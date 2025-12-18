8183df52f84b9f5a53330a0dba57478c96f4d3a8
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-18 08:21:24 +0530

    Added code for split array in largest subarray sum

---
+++ 02 - Binary Search/23 - Split Array in largest sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        if(nums.length < k){
+            return -1;
+        }
+        int low = nums[0];
+        int high = nums[0];
+        for(int i = 1; i<nums.length; ++i){
+            low = Math.max(low, nums[i]);
+            high += nums[i];
+        }
+        int ans = -1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            int count = 1;
+            int tempSum = 0;
+            for(int i = 0; i<nums.length; ++i){
+                if(tempSum + nums[i] <= mid){
+                    tempSum += nums[i];
+                }else{
+                    ++count;
+                    tempSum = nums[i];
+                }
+            }
+            if(count <= k){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
+
+// TC - O(Nlog(Sum Of Elements))
+// SC - O(1)
