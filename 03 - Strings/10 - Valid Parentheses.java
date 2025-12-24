31cb5b0ceb67f1cb827cbc50a9dd02baa827d88b
soumya007GitHub <soumyatripathy2021@gmail.com>
2025-12-24 13:26:23 +0530

    added code for valid parentheses

---
+++ 03 - Strings/10 - Valid Parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<s.length(); ++i){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(ch == ')' && stack.peek() == '('){
+                    stack.pop();
+                }
+                else if(ch == '}' && stack.peek() == '{'){
+                    stack.pop();
+                }
+                else if(ch == ']' && stack.peek() == '['){
+                        stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
+
+// TC - O(N)
+// SC - O(N)
