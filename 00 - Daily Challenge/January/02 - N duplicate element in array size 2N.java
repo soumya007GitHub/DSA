b3fd53b967faf986014e7c4dd7b0be4897b91ad9
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-01-02 11:47:58 +0530

    added code for N duplicate element in an array of size 2N

---
+++ 00 - Daily Challenge/January/02 - N duplicate element in array size 2N.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length; ++i){
+            if(set.contains(nums[i])){
+                return nums[i];
+            }
+            set.add(nums[i]);
+        }
+        return -1;
+    }
+}
+
+// TC - O(N)
+// SC - O(N)
