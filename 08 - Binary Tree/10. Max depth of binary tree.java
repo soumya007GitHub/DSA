056567efe25ade957573099b29bfef5ae1a3ecbb
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-02-28 21:21:49 +0530

    Added correct for max depth of binary tree

--- 08 - Binary Tree/10. Max depth of binary tree.java
+++ 08 - Binary Tree/10. Max depth of binary tree.java
@@ -3,7 +3,7 @@ class Solution {
         if(root == null){
             return 0;
         }
-        return Math.max(1+maxDepth(root.left), 1+maxDepth(root.right));
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
     }
 }
 
