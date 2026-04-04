69e82fa383ceb28b8c6c12a127e36a0aad687978
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-04-04 23:08:13 +0530

    Added code for climbing stairs

---
+++ 12 - DP/02. Climbing Stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 3){
+            return n;
+        }
+        int first = 1;
+        int second = 2;
+        for(int i = 3; i<=n; ++i){
+            int temp = first+second;
+            first = second;
+            second = temp;
+        }
+        return second;
+    }
+}
+
+
+// TC - O(N)
+// SC - O(1)
