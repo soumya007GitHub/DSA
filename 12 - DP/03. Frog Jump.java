38f614f7526592b0070cb2066a6c407b6d967587
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-04-04 23:42:22 +0530

    Added optimised code for frog jump

--- 12 - DP/03. Frog Jump.java
+++ 12 - DP/03. Frog Jump.java
@@ -17,3 +17,27 @@ class Solution {
 
 // TC - O(N)
 // SC - O(N)
+
+
+// Space optimised code
+
+class Solution {
+    public int frogJump(int[] heights) {
+        int[] dp = new int[heights.length];
+        int initial = 0;
+        int initial2 = 0;
+        for(int i = 1; i<heights.length; ++i){
+            int stepOne = initial2 + Math.abs(heights[i-1] - heights[i]);
+            int stepTwo = Integer.MAX_VALUE;
+            if(i != 1){
+                stepTwo = initial + Math.abs(heights[i-2]-heights[i]);
+            }
+            initial = initial2;
+            initial2 = Math.min(stepOne, stepTwo);
+        }
+        return initial2;
+    }
+}
+
+// TC - O(N)
+// SC - O(1)
