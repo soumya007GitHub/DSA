380c57ef0e590f1dd083960b0634681cd9275d94
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-04-15 22:43:13 +0530

    Added code for buy and sell stock 2

---
+++ 12 - DP/15. Buy and Sell stock 2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        for(int i = 0; i<prices.length; ++i){
+            for(int j = 0; j<2; ++j){
+                dp[i][j] = -1;
+            }
+        }
+        return helper(0, 0, prices, dp);
+    }
+    public int helper(int index, int buyStatus, int[] prices, int[][] dp){
+        if(index == prices.length) return 0;
+        if(dp[index][buyStatus] != -1) return dp[index][buyStatus];
+        int ans = Integer.MIN_VALUE;
+        // Didn't purchased any other stock previously
+        if(buyStatus == 0){
+            // purchase current stock
+            int buy = -prices[index] + helper(index+1, 1, prices, dp);
+            // skip current stock
+            int notBuy = helper(index+1, 0, prices, dp);
+            ans = Math.max(buy, notBuy);
+        }
+        // purchased any other stock previously
+        else{
+            // sell stock
+            int sell = prices[index] + helper(index+1, 0, prices, dp);
+            // dont sell stock
+            int notSell = helper(index+1, 1, prices, dp);
+            ans = Math.max(sell, notSell);
+        }
+        dp[index][buyStatus] = ans;
+        return dp[index][buyStatus];
+    }
+}
+
+// TC - O(N*2)
+// SC - O(N*2)
