69acc0b7a46bad1a0b3851acc1a66a8ca5c830d3
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-04-19 21:07:52 +0530

    Added code for coin change

---
+++ 12 - DP/24. Coin change 1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i = 0; i<coins.length+1; ++i){
+            dp[i][0] = 0;
+        }
+        for(int i = 1; i<=amount; ++i){
+            dp[0][i] = Integer.MAX_VALUE;
+        }
+        for(int i = 1; i<=coins.length; ++i){
+            for(int j = 1; j<=amount; ++j){
+                if(coins[i-1] <= j){
+                    if(dp[i][j - coins[i - 1]] != Integer.MAX_VALUE){
+                        dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+                    }else{
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount] >= Integer.MAX_VALUE ? -1 : dp[coins.length][amount];
+    }
+}
+// TC = O(m*n)
+// SC = O(m*n)
